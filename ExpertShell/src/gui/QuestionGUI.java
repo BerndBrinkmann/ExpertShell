f5cf643e87079837c4ce83c42064f1bc56f4193b
mandy <mandy@mandy-PC>
2014-08-20 10:37:08 +1000

    stuff for user gui

---
+++ ExpertShell/src/gui/QuestionGUI.java
@@ -0,0 +1,18 @@
+package gui;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+
+
+public class QuestionGUI {
+	
+	public SelectionAdapter selAdaptor;
+	
+	public QuestionGUI(Composite CompQ){
+		
+	}
+
+}
