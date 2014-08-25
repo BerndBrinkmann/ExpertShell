05ba2b700be669323b6a8cce93eb93459abd6970
mandy <mandy@mandy-PC>
2014-08-25 18:13:30 +1000

    pop up dialogs for run errors

---
+++ ExpertShell/src/gui/NoRun.java
@@ -0,0 +1,69 @@
+package gui;
+
+import org.eclipse.swt.widgets.Dialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.eclipse.swt.events.ControlAdapter;
+import org.eclipse.swt.events.ControlEvent;
+
+public class NoRun extends Dialog {
+
+	protected Object result;
+	protected Shell shlError;
+
+	/**
+	 * Create the dialog.
+	 * @param parent
+	 * @param style
+	 */
+	public NoRun(Shell parent, int style) {
+		super(parent, style);
+		setText("SWT Dialog");
+	}
+
+	/**
+	 * Open the dialog.
+	 * @return the result
+	 */
+	public Object open() {
+		createContents();
+		shlError.open();
+		shlError.layout();
+		Display display = getParent().getDisplay();
+		while (!shlError.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Create contents of the dialog.
+	 */
+	private void createContents() {
+		shlError = new Shell(getParent(), SWT.DIALOG_TRIM);
+		shlError.addControlListener(new ControlAdapter() {
+			@Override
+			public void controlResized(ControlEvent e) {
+				e.getSource();
+			}
+		});
+		shlError.setImage(SWTResourceManager.getImage(NoRun.class, "/resources/exclamation_sign.png"));
+		shlError.setSize(365, 150);
+		shlError.setText("Error");
+		
+		Composite composite = new Composite(shlError, SWT.NONE);
+		composite.setBounds(0, 0, 444, 148);
+		
+		Label lblNoKnowledgeBase = new Label(composite, SWT.NONE);
+		lblNoKnowledgeBase.setBackground(SWTResourceManager.getColor(SWT.COLOR_LIST_SELECTION_TEXT));
+		lblNoKnowledgeBase.setText("Unable to Run. Please select a knowledgebase");
+		lblNoKnowledgeBase.setBounds(10, 10, 341, 105);
+
+	}
+}
